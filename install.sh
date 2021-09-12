c3d4316b591b4ef14a3785cd6e11338513ac6628
Nander <nander@nander.dev>
2021-09-12 13:43:06 +0000

    now uses antigen and with p10k config

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
 sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
 sudo fc-cache -f -v
 
-# ZSH Stuff
-cp -f ~/dotfiles/.zshrc ~/.zshrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# antigen
+mkdir -p ~/.config/antigen
+curl -L git.io/antigen > ~/.config/antigen/antigen.zsh
