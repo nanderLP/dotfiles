988c8fb1c504215316d85dd015f6b9d9755f42bb
Nander <nander@nander.dev>
2021-09-12 13:43:42 +0000

    format

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
 sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
 sudo fc-cache -f -v
 
-
 # antigen
 mkdir -p ~/.config/antigen
 curl -L git.io/antigen > ~/.config/antigen/antigen.zsh
+
+mv ./.zshrc ~
+mv ./.p10k.zsh ~
